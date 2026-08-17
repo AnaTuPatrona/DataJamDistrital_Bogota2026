965d3c5b56640e8e0636c30ea5b67f9291d66e4b
AnaTuPatrona <111031121+AnaTuPatrona@users.noreply.github.com>
2026-08-17 14:48:35 -0500

    el de violencia intrafamiliar pesa mucho
    pesa mucho entonces toca obtenerlo por el link, k es este del script

---
+++ scripts/open_csv_via_link.py
@@ -0,0 +1,5 @@
+import pandas as pd
+url_csv = "https://datosabiertos.bogota.gov.co/dataset/a1e1ef90-10c0-436f-a290-d1f7c1cf2242/resource/bbe9e920-c564-40f2-a307-5ae1ff087168/download/osb_saludmental-vintrafamiliar.csv"
+
+df = pd.read_csv(url_csv, delimiter=';', encoding='UTF-8-SIG', decimal=',')
+print(df.head())
